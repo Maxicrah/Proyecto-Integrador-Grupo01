f47beb0587356e88701579b7788f7e555c7c757d
carlos8788 <carlos8788@gmail.com>
2023-08-09 18:36:59 -0300

    header and footer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+
+
+app.use(express.static(__dirname+'\\public'));
 
 const PORT = 3000
 
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname + '/views/home.html');
+// });
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + '/views/headerFooter.html');
 });
 
 app.get('/register', (req,res)=>{
