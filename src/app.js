f7b5ff4b741aa340442d5f0b3d070a67a439b9e0
carlos8788 <carlos8788@gmail.com>
2023-08-10 10:55:25 -0300

    implementando ejs

--- src/app.js
+++ src/app.js
@@ -1,35 +1,43 @@
 const express = require('express');
 const app = express();
-
-
 app.use(express.static('public'));
+
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 const PORT = 3000
 
 // app.get('/', (req,res)=>{
 //     res.sendFile(__dirname + '/views/home.html');
 // });
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/headerFooter.html');
+app.get('/', (req, res) => {
+    res.render('index', {'title': 'index.ejs'});
 });
 
-app.get('/productDetail', (req,res)=>{
+
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname + '/views/headerFooter.html');
+// });
+
+app.get('/productDetail', (req, res) => {
     res.sendFile(__dirname + '/views/productDetail.html');
 });
 
-app.get('/productCart', (req,res)=>{
+app.get('/productCart', (req, res) => {
     res.sendFile(__dirname + '/views/productCart.html');
 });
 
-app.get('/register', (req,res)=>{
+app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 });
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`PORT: ${PORT}`);
 });
