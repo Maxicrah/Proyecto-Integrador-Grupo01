90ba6dd9c29dbb683da64cfa106cd2d2460e31af
Maxicrah <maximaxmaxii690@gmail.com>
2023-08-22 10:43:29 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const routerMain = require('./src/routes/mainRouter')
+const routerMain = require('./routes/mainRouter')
 
 app.use(express.static('public'));
 
